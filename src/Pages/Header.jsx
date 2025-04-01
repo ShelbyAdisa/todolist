bcef1c4b22bf0254d9be1368843d4e8e614403e4
ShelbyAdisa <sheladisaa@gmail.com>
2025-04-01 19:02:20 +0300

    todolist

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="py-6 mb-6 border-b border-pink-300">
-      <h1 className="text-3xl font-bold text-center text-pink-600">Task Manager</h1>
+      <h1 className="text-3xl font-bold text-center text-pink-600">TASK MANAGER</h1>
     </header>
   );
 };
